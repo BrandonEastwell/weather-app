06935e3c63619b95a9533c6c57bf626a0c3b1232
Brandon Eastwell <be21160@essex.ac.uk>
2025-01-06 18:17:32 +0000

    functions and input forms to request data from weather api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,34 @@
 import "../styles/normalize.css";
 import "../styles/styles.css";
+
+const API_KEY = "LBECU2EPA4ANQ9PMZJUSQTQ9S";
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+searchInput.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  if (event.key === 'Enter') {
+    handleSearch();
+  }
+});
+
+searchButton.addEventListener('click', handleSearch);
+
+async function getWeatherData(location) {
+  const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
+  try {
+    const response = await fetch(URL, {mode: "cors"});
+    const json = await response.json();
+    console.log(json);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function handleSearch() {
+  const value = searchInput.value.trim().toLowerCase();
+  if (value !== '') {
+    getWeatherData(value);
+  }
+}
